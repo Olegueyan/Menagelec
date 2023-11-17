3c04164eaf9738f676b85ab7aac17f212f70192b
Olegueyan <gsourtg2cer@gmail.com>
2023-11-17 16:44:40 +0100

    LigneCommandeRepository - Update => allow to read all lines

--- Repository/LigneCommandeRepository.cs
+++ Repository/LigneCommandeRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Menagelec.Entity;
 using Menagelec.Service;
@@ -10,6 +11,7 @@ public static class LigneCommandeRepository
     // TODO : Specific - Read, Delete and Update
     
     private const string QueryInsertion = "INSERT INTO lignecommande (produit, commande, quantite) VALUES (@produit, @commande, @quantite)";
+    private const string QuerySelectAll = "SELECT * FROM lignecommande";
     
     public static async Task<int> Create(LigneCommande ligneCommande)
     {
@@ -22,5 +24,25 @@ public static class LigneCommandeRepository
         var result = await command.ExecuteNonQueryAsync();
         await connection.CloseAsync();
         return result;
+    }
+
+    public static async Task<Collection<LigneCommande>> ReadAll()
+    {
+        var connection = DatabaseService.GetConnection();
+        await connection.OpenAsync();
+        var ligneCommandes = new Collection<LigneCommande>();
+        var command = new MySqlCommand(QueryInsertion, connection);
+        var reader = await command.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            ligneCommandes.Add(new LigneCommande
+            {
+                Produit = reader.GetInt32(0),
+                Commande = reader.GetInt32(1),
+                Quantite = reader.GetInt32(2)
+            });
+        }
+        await connection.CloseAsync();
+        return ligneCommandes;
     }
 }
