6d0f19e83bcc9cf409be621ca24bf25ea92b21cb
Olegueyan <gsourtg2cer@gmail.com>
2023-11-17 13:57:05 +0100

    Add Equals/ToString/GetHashCode methods to Client

--- Entity/Client.cs
+++ Entity/Client.cs
@@ -73,5 +73,45 @@ namespace Menagelec.Entity
             get => _tel;
             set => _tel = value;
         }
+
+        public override bool Equals(object obj)
+        {
+            // Vérifier si l'objet en paramètre est nul ou n'est pas du type Client
+            if (obj is Client otherClient)
+            {
+                // Comparer chaque attribut
+                return
+                    _idClient == otherClient._idClient &&
+                    _civilite == otherClient._civilite &&
+                    _nom == otherClient._nom &&
+                    _prenom == otherClient._prenom &&
+                    _adresse == otherClient._adresse &&
+                    _ville == otherClient._ville &&
+                    _cp == otherClient._cp &&
+                    _mail == otherClient._mail &&
+                    _tel == otherClient._tel;
+            }
+            return false;
+        }
+
+        // ChatGPT
+        public override int GetHashCode()
+        {
+            // Utiliser XOR (^) pour combiner les codes de hachage des attributs
+            return _idClient.GetHashCode() ^
+                   (Civilite?.GetHashCode() ?? 0) ^
+                   (Nom?.GetHashCode() ?? 0) ^
+                   (Prenom?.GetHashCode() ?? 0) ^
+                   (Adresse?.GetHashCode() ?? 0) ^
+                   (Ville?.GetHashCode() ?? 0) ^
+                   (Cp?.GetHashCode() ?? 0) ^
+                   (Mail?.GetHashCode() ?? 0) ^
+                   (Tel?.GetHashCode() ?? 0);
+        }
+        
+        public override string ToString()
+        {
+            return $"Client {{ IdClient: {_idClient}, Civilite: {_civilite}, Nom: {_nom}, Prenom: {_prenom}, Adresse: {_adresse}, Ville: {_ville}, Cp: {_cp}, Mail: {_mail}, Tel: {_tel} }}";
+        }
     }
 }
