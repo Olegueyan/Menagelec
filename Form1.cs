61fd044d10601cc1a1f637433b02c78b4f7d0c0c
Olegueyan <gsourtg2cer@gmail.com>
2023-11-17 08:06:18 +0100

    Initial commit

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace Menagelec
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
     }
-  }
 }
