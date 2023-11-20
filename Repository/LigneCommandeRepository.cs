70736aca0b20c852809e8b88539228470cd861e0
Olegueyan <gsourtg2cer@gmail.com>
2023-11-20 08:37:19 +0100

    Delete ReadAll to LigneCommandeRepository

--- Repository/LigneCommandeRepository.cs
+++ Repository/LigneCommandeRepository.cs
@@ -11,7 +11,6 @@ public static class LigneCommandeRepository
     // TODO : Specific - Read, Delete and Update
     
     private const string QueryInsertion = "INSERT INTO lignecommande (produit, commande, quantite) VALUES (@produit, @commande, @quantite)";
-    private const string QuerySelectAll = "SELECT * FROM lignecommande";
     
     public static async Task<int> Create(LigneCommande ligneCommande)
     {
@@ -24,25 +23,5 @@ public static class LigneCommandeRepository
         var result = await command.ExecuteNonQueryAsync();
         await connection.CloseAsync();
         return result;
-    }
-
-    public static async Task<Collection<LigneCommande>> ReadAll()
-    {
-        var connection = DatabaseService.GetConnection();
-        await connection.OpenAsync();
-        var ligneCommandes = new Collection<LigneCommande>();
-        var command = new MySqlCommand(QuerySelectAll, connection);
-        var reader = await command.ExecuteReaderAsync();
-        while (await reader.ReadAsync())
-        {
-            ligneCommandes.Add(new LigneCommande
-            {
-                Produit = reader.GetInt32(0),
-                Commande = reader.GetInt32(1),
-                Quantite = reader.GetInt32(2)
-            });
-        }
-        await connection.CloseAsync();
-        return ligneCommandes;
     }
 }
