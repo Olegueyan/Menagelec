e063de4052b3a18a6099b11d0445208e775e5885
Olegueyan <gsourtg2cer@gmail.com>
2023-12-01 15:39:01 +0100

    packaging list feature

--- Component/PdfPackagingListGenerator.cs
+++ Component/PdfPackagingListGenerator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Menagelec.Data;
